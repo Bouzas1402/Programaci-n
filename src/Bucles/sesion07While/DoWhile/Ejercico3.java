3164606b2f6e555449996d41c5a45b07455bd96c
CarlosBouzasClase <carlos.bouzas@educa.madrid.org>
2020-11-17 13:53:06 +0100

    Ejercicios 2 y 2 de while/do while

---
+++ src/Bucles/sesion07While/DoWhile/Ejercico3.java
@@ -0,0 +1,14 @@
+package Bucles.sesion07While.DoWhile;
+
+import java.util.Scanner;
+
+public class Ejercico3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+    }
+}
